afddef21804dd77f0a37cfaf2e242595bd79ebc3
Idean Labib <idean1.labib@gmail.com>
2020-04-24 22:29:30 -0700

    Fix typo

--- project/eval.py
+++ project/eval.py
@@ -131,7 +131,8 @@ def main(bow=False, discriminator=False, perplexity=True, **kwargs):
     if perplexity:
         perplexities = perplexity_loop(bow=bow, **kwargs)
         print("Running perplexities with parameters: ", kwargs)
-        with open(kwargs.get('out_file', './eval/perplexity.json'), 'w') as f:
+        out_file = kwargs.get('out_file', './eval/perplexity.json')
+        with open(out_file, 'w') as f:
             print(perplexities)
             json.dump(perplexities, f)
         print(f"Perplexities outputted to {out_file}")
